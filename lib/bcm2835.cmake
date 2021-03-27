79af9e4957cb66ed07c0ee2e3d75da049887a860
fantasticmao <maomao8017@gmail.com>
2021-03-27 19:44:14 +0800

    :rabbit:
    1. update CMakeLists.txt

--- lib/bcm2835.cmake
+++ lib/bcm2835.cmake
@@ -1,25 +1,32 @@
-# https://cmake.org/cmake/help/latest/module/ExternalProject.html
-include(ExternalProject)
+# https://cmake.org/cmake/help/latest/module/FetchContent.html
+include(FetchContent)
 
-ExternalProject_Add(bcm2835_fetch
-    PREFIX              "bcm2835-1.68"
-    URL                 http://www.airspayce.com/mikem/bcm2835/bcm2835-1.68.tar.gz
-    URL_MD5             96ee0c2b078a77bfe4aa749fd056157a
-    CONFIGURE_COMMAND   "./configure"
-    BUILD_IN_SOURCE     true
-    BUILD_COMMAND       "make"
-    INSTALL_COMMAND     ""
+FetchContent_Declare(
+    bcm2835
+    URL         http://www.airspayce.com/mikem/bcm2835/bcm2835-1.68.tar.gz
+    URL_MD5     96ee0c2b078a77bfe4aa749fd056157a
 )
 
-ExternalProject_Get_Property(bcm2835_fetch SOURCE_DIR)
+if(NOT bcm2835_POPULATED)
+    message(NOTICE "-- Download bcm2835 ing ...")
+    FetchContent_Populate(bcm2835)
+    message(NOTICE "-- Download bcm2835 complete!")
+    message(NOTICE "-- Build bcm2835 ing ...")
+    # https://cmake.org/cmake/help/latest/command/execute_process.html
+    execute_process(
+        COMMAND             "./configure"
+        COMMAND             "make"
+        WORKING_DIRECTORY   ${bcm2835_SOURCE_DIR}
+        RESULT_VARIABLE     bcm2835_RESULT
+    )
+    message(NOTICE "-- Build bcm2835 complete! execute result: ${bcm2835_RESULT}.")
+endif()
 
 # https://cmake.org/cmake/help/latest/guide/importing-exporting/index.html
 add_library(bcm2835 STATIC IMPORTED)
 set_target_properties(bcm2835 PROPERTIES
-    # https://cmake.org/cmake/help/v3.20/manual/cmake-properties.7.html#properties-on-targets
-    IMPORTED_LOCATION       ${SOURCE_DIR}/src/libbcm2835.a
-    INCLUDE_DIRECTORIES     ${SOURCE_DIR}/src/
+    # https://cmake.org/cmake/help/latest/manual/cmake-properties.7.html#properties-on-targets
+    IMPORTED_LOCATION               ${bcm2835_SOURCE_DIR}/src/libbcm2835.a
+    INTERFACE_INCLUDE_DIRECTORIES   ${bcm2835_SOURCE_DIR}/src/
 )
-
-add_dependencies(bcm2835 bcm2835_fetch)
 
